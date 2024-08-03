4bb3ef161bfdc0b07a1b097ef301ee872b8b9b6d
msuryrsa@gmail.com <msuryrsa@gmail.com>
2024-08-04 03:08:01 +0900

    userRef null bug fixed

--- app/deck_viewer/page.tsx
+++ app/deck_viewer/page.tsx
@@ -2,21 +2,24 @@
 
 import Deck_img_maker from '@/components/deck_img_maker'
 import Hero from '@/components/hero'
-import html2canvas from 'html2canvas';
 import React, { useRef } from 'react';
 
 export default function Deck_viewer() {
   const divRef = useRef<HTMLDivElement>();
 
   const handleDownload = async () => {
-    const canvas = await html2canvas(divRef.current);
-    const link = document.createElement('a');
-    link.href = canvas.toDataURL('image/png');
-    link.download = 'screenshot.png';
-    link.click();
-  };
-
-
+    if (divRef.current) {
+      // `html2canvas` をクライアントサイドでのみインポート
+      const html2canvas = (await import('html2canvas')).default;
+      const canvas = await html2canvas(divRef.current);
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'screenshot.png';
+      link.click();
+    } else {
+      console.error('divRef is null');
+    }
+  }
   return (
     <>
     <h1>this is a new page</h1>
