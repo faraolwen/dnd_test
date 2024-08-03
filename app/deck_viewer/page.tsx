c106b7f31b3409241cfca7616f3cff9d4e694581
msuryrsa@gmail.com <msuryrsa@gmail.com>
2024-08-04 03:32:09 +0900

    userRef null bug fixed

--- app/deck_viewer/page.tsx
+++ app/deck_viewer/page.tsx
@@ -10,19 +10,15 @@ export default function Deck_viewer() {
   const handleDownload = async () => {
     const divRef = useRef<HTMLDivElement>(null);
 
-    if (divRef.current) {
-      
-      // `html2canvas` をクライアントサイドでのみインポート
+
       const html2canvas = (await import('html2canvas')).default;
       const canvas = await html2canvas(divRef.current);
       const link = document.createElement('a');
       link.href = canvas.toDataURL('image/png');
       link.download = 'screenshot.png';
       link.click();
-    } else {
-      console.error('divRef is null');
-    }
-  }
+
+  
   return (
     <>
     <h1>this is a new page</h1>
